24dc97103e39930ddf1332026d0c57658339548f
Nicolas <kolya.vladimirsky@gmail.com>
2025-06-20 01:18:06 +0300

    init

---
+++ rebuild.bat
@@ -0,0 +1,2 @@
+call vcvars64.bat && rmdir /s /q build && mkdir build && cmake -G Ninja -DMORT_DEV=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -B ./build -S .
+cmake --build ./build -j 10 && copy build\compile_commands.json .\
---
+++ src/vec.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace mort
+{
+class vec
+{
+public:
+    void func()
+    {
+        int a = 2 + 34;
+    }
+};
+}
